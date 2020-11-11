2ce773ea27d3178566aff4bdd991a449e976d39a
Bilal Sevinc <bilalresmi@gmail.com>
2020-11-11 05:23:00 +0300

    removed unused components

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import './App.scss';
 /* pages */
 import home from './pages/home/home';
 import hakkimizda from './pages/hakkimizda/hakkimizda';
-import bulutfilo from './pages/bulutfilo/bulutfilo';
-import stratejikplan from './pages/stratejikplan/stratejikplan';
-import drive from './pages/drive/drive';
-import scada from './pages/scada/scada';
 import iletisim from './pages/iletisim/iletisim';
 
 import Hizmetlerimiz from './pages/hizmetlerimiz/hizmetlerimiz';
@@ -18,18 +14,6 @@ import ProjeYonetimi from './pages/cozumlerimiz/ProjeYonetimi';
 import TeknolojiYonetimi from './pages/cozumlerimiz/TeknolojiYonetimi';
 import VatandasDeneyimi from './pages/cozumlerimiz/VatandasDeneyimi';
 
-
-import parmakizi from './pages/parmakizi/parmakizi';
-import sahaurun from './pages/sahaurun/sahaurun';
-import eksigorta from './pages/eksigorta/eksigorta';
-
-/* inner pages */
-/* import catiSantral from './pages/dijitalcati/catiSantral';
-import catiSesTanima from './pages/dijitalcati/catiSesTanima';
-import catiGiys from './pages/dijitalcati/catiGiys';
-import catiGys from './pages/dijitalcati/catiGys';
-import catiIk from './pages/dijitalcati/catiIK';
-import catiDys from './pages/dijitalcati/catiDys'; */
 
 /* components */
 import Navbar from './components/Navbar/Navbar';
@@ -50,10 +34,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={home}/>
         <Route exact path="/hakkimizda" component={hakkimizda}/>
-        <Route exact path="/bulutfilo" component={bulutfilo}/>
-        <Route exact path="/stratejikplan" component={stratejikplan}/>
-        <Route exact path="/drive" component={drive}/>
-        <Route exact path="/scada" component={scada}/>
+
         <Route exact path="/iletisim" component={iletisim}/>
 
         <Route  path="/hizmetlerimiz" component={Hizmetlerimiz}/>
@@ -64,16 +45,6 @@ function App() {
         <Route exact path="/teknoloji-yonetimi" component={TeknolojiYonetimi}/>
         <Route exact path="/vatandas-deneyimi-yonetimi" component={VatandasDeneyimi}/>
 
-        <Route exact path="/parmakizi" component={parmakizi}/>
-        <Route exact path="/noktabakim" component={sahaurun}/>
-        <Route exact path="/eksigorta" component={eksigorta}/>
-        {/* inner routes */}
-{/*         <Route exact path="/cati/santral" component={catiSantral}/>
-        <Route exact path="/cati/sestanima" component={catiSesTanima}/>
-        <Route exact path="/cati/giys" component={catiGiys}/>
-        <Route exact path="/cati/gys" component={catiGys}/>
-        <Route exact path="/cati/ik" component={catiIk}/>
-        <Route exact path="/cati/dys" component={catiDys}/> */}
         
       </Switch>
       <Footer/>
